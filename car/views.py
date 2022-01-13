9d969a22b8d214d0a8e7fdba757078c3985ae897
ht21992 <47816410+ht21992@users.noreply.github.com>
2022-01-13 19:30:26 +0400

    views.py updated

--- car/views.py
+++ car/views.py
@@ -6,8 +6,6 @@ import json
 def main(request):
     json_data = list(Car.objects.values())
     manufacturer_number = len(Car.objects.values('make',).distinct().order_by())
-    # color_number = len(Car.objects.values('color',).distinct().order_by())
-    # checkbox_number = manufacturer_number + color_number
     cars = Car.objects.all()
     context ={'cars':cars, 'json_data': json.dumps(json_data),'checkbox_number':manufacturer_number}
     return render(request, 'car/main.html',context= context)
